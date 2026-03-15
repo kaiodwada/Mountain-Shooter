03aef7a40ecf409507d8a4fcecec3856a4f3cb64
Kaioadwada <kaio-giovany@hotmail.com>
2026-03-15 16:55:51 -0300

    Hit box Collisions HUD SCORE

--- code/Const.py
+++ code/Const.py
@@ -1,11 +1,12 @@
 # C
 import pygame
 
-COLOR_ORANGE = (255, 128, 0)
-COLOR_RED = (255, 0, 0)
-COLOR_WHITE = (255, 255, 255)
-COLOR_YELLOW = (255, 255, 0)
-
+C_ORANGE = (255, 128, 0)
+C_RED = (255, 0, 0)
+C_WHITE = (255, 255, 255)
+C_YELLOW = (255, 255, 0)
+C_GREEN = (0, 128, 0)
+C_CYAN = (0, 128, 128)
 # M
 MENU_OPTION = ('NEW GAME 1P',
                'NEW GAME 2P - COOPERATIVE',
@@ -35,9 +36,9 @@ ENTITY_SPEED = {
     'Player1Shot': 1,
     'Player2': 3,
     'Player2Shot': 1,
-    'Enemy1':1,
+    'Enemy1': 1,
     'Enemy1Shot': 5,
-    'Enemy2':1,
+    'Enemy2': 1,
     'Enemy2Shot': 2,
 }
 
@@ -65,15 +66,51 @@ ENTITY_SHOT_DELAY = {
     'Enemy1': 100,
     'Enemy2': 200,
 }
+
+ENTITY_DAMAGE = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    'Player1': 1,
+    'Player1Shot': 25,
+    'Player2': 1,
+    'Player2Shot': 20,
+    'Enemy1': 1,
+    'Enemy1Shot': 20,
+    'Enemy2': 1,
+    'Enemy2Shot': 15,
+}
+
+ENTITY_SCORE = {
+    'Level1Bg0': 0,
+    'Level1Bg1': 0,
+    'Level1Bg2': 0,
+    'Level1Bg3': 0,
+    'Level1Bg4': 0,
+    'Level1Bg5': 0,
+    'Level1Bg6': 0,
+    'Player1': 0,
+    'Player1Shot':0,
+    'Player2': 0,
+    'Player2Shot': 0,
+    'Enemy1': 100,
+    'Enemy1Shot': 0,
+    'Enemy2': 125,
+    'Enemy2Shot': 0,
+}
+
 # P
 PLAYER_KEY_UP = {'Player1': pygame.K_UP,
                  'Player2': pygame.K_w}
 PLAYER_KEY_DOWN = {'Player1': pygame.K_DOWN,
-                 'Player2': pygame.K_s}
+                   'Player2': pygame.K_s}
 PLAYER_KEY_LEFT = {'Player1': pygame.K_LEFT,
-                 'Player2': pygame.K_a}
+                   'Player2': pygame.K_a}
 PLAYER_KEY_RIGHT = {'Player1': pygame.K_RIGHT,
-                 'Player2': pygame.K_d}
+                    'Player2': pygame.K_d}
 PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
-                 'Player2': pygame.K_LCTRL}
-
+                    'Player2': pygame.K_LCTRL}
