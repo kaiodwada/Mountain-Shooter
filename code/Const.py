f1fab8f71ac4fa9bbe01dff6e989a33280c8ea87
Kaioadwada <kaio-giovany@hotmail.com>
2026-03-15 21:27:21 -0300

    SQLite3 implementation Adding proxy pattern

--- code/Const.py
+++ code/Const.py
@@ -20,6 +20,21 @@ WIN_HEIGHT = 324
 
 # S
 SPAWN_TIME = 4000
+SCORE_POS = {'Title': (WIN_WIDTH / 2, 50),
+             'Enter name': (WIN_WIDTH / 2, 80),
+             'Label': (WIN_WIDTH / 2, 90),
+             'Name': (WIN_WIDTH / 2, 110),
+             0: (WIN_WIDTH / 2, 110),
+             1: (WIN_WIDTH / 2, 130),
+             2: (WIN_WIDTH / 2, 150),
+             3: (WIN_WIDTH / 2, 170),
+             4: (WIN_WIDTH / 2, 190),
+             5: (WIN_WIDTH / 2, 210),
+             6: (WIN_WIDTH / 2, 230),
+             7: (WIN_WIDTH / 2, 250),
+             8: (WIN_WIDTH / 2, 270),
+             9: (WIN_WIDTH / 2, 290),
+             }
 
 # E
 EVENT_ENEMY = pygame.USEREVENT + 1
@@ -114,7 +129,7 @@ ENTITY_SCORE = {
     'Level2Bg3': 0,
     'Level2Bg4': 0,
     'Player1': 0,
-    'Player1Shot':0,
+    'Player1Shot': 0,
     'Player2': 0,
     'Player2Shot': 0,
     'Enemy1': 100,
@@ -136,5 +151,7 @@ PLAYER_KEY_SHOOT = {'Player1': pygame.K_RCTRL,
                     'Player2': pygame.K_LCTRL}
 
 # T
-TIMEOUT_LEVEL = 20000 # 20s
-TIMEOUT_STEP = 100 # 100mls
+TIMEOUT_LEVEL = 20000  # 20s
+TIMEOUT_STEP = 100  # 100mls
+
+
